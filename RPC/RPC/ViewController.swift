23ab9398074cd34e6d627ba9ea84a6b38f6eaf2a
krisrimar <rimarkris@gmail.com>
2017-01-28 17:07:06 +0200

    Add outlets and actions to Main screen
    - Add updateUI() function that makes changes in the UI elements based
    on the currentGameStatus

--- RPC/RPC/ViewController.swift
+++ RPC/RPC/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentGameState = GameState.start
 
+    @IBOutlet weak var appSign: UILabel!
+    @IBOutlet weak var gameStatusLabel: UILabel!
+    @IBOutlet weak var rockSignBtn: UIButton!
+    @IBOutlet weak var paperSignBtn: UIButton!
+    @IBOutlet weak var scissorsSignBtn: UIButton!
+    @IBOutlet weak var playAgainBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rockBtnTapped(_ sender: Any) {
+        updateUI()
+    }
+    @IBAction func paperBtnTapped(_ sender: Any) {
+        updateUI()
+    }
+    @IBAction func scissorsBtnTapped(_ sender: Any) {
+        updateUI()
+    }
+    @IBAction func playAgainBtnTapped(_ sender: Any) {
+        updateUI()
+    }
+    
+    
+    func updateUI() {
+        if(currentGameState == .start) {
+            gameStatusLabel.text = currentGameState.result
+            appSign.text = "🤖"
+            playAgainBtn.isHidden = true
+            
+            rockSignBtn.isHidden = false
+            paperSignBtn.isHidden = false
+            scissorsSignBtn.isHidden = false
+            
+            rockSignBtn.isEnabled = true
+            paperSignBtn.isEnabled = true
+            scissorsSignBtn.isEnabled = true
+        }
+    }
 
 }
 
