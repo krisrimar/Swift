6c14b75e369a5ae8614cf4f26f05e86626ae251e
krisrimar <rimarkris@gmail.com>
2017-02-08 22:12:23 +0200

    Initial commit
    MainVC:
    
    - Add ImageView at top for main band image
    - Add view with 3 buttons
    - Add TableView for content
    - Add one table cell with song image and title
    
    VideoVC:
    
    - Add WebView for video from internet
    
    General:
    
    - Add custom font “Oswald Light” to project

---
+++ PartyRockApp/PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Kris Rimar on 8/2/17.
+//  Copyright © 2017 Kris Rimar. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ PartyRockApp/PartyRockApp/VideoVC.swift
@@ -0,0 +1,19 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by Kris Rimar on 8/2/17.
+//  Copyright © 2017 Kris Rimar. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
