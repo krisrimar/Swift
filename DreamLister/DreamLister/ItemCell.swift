858d3e82a749be3c0251c4286cdb51ba89fc9c29
krisrimar <rimarkris@gmail.com>
2017-02-12 19:05:52 +0200

    Add ItemCell class
    MainVC:
    
    - Add code necessary for table view to function
    
    ItemCell:
    
    - Connect outlets

---
+++ DreamLister/DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Kris Rimar on 12/2/17.
+//  Copyright © 2017 Kris Rimar. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
